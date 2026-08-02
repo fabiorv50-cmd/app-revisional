9f987c6d0ef34f3c8fdcb8d2c24cf42211410449
fabiorv50 <fabiorv50@gmail.com>
2026-08-02 19:20:30 -0300

    Sincronizando main universal e pacote core

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-# main.py
+import os
 import sys
 
-# Escolha o modo de execução: "desktop" ou "flet"
-MODO_EXECUCAO = "desktop"
+# Detecta se está rodando dentro do aplicativo Android
+IS_MOBILE = (
+        "ANDROID_ARGUMENT" in os.environ
+        or "PYTHONOPTIMIZE" in os.environ
+        or "ANDROID_ROOT" in os.environ
+)
 
-if __name__ == "__main__":
-    if MODO_EXECUCAO == "desktop":
+if IS_MOBILE:
+    # --- AMBIENTE ANDROID (CELULAR) ---
+    import flet as ft
+    from ui_flet import main_flet
+
+    ft.app(target=main_flet)
+
+else:
+    # --- AMBIENTE DESKTOP (WINDOWS / PYCHARM) ---
+    try:
         from ui_desktop import AppDesktop
+
         app = AppDesktop()
         app.mainloop()
-    else:
+    except Exception as e:
+        # Caso falhar o desktop no PC, ele usa o Flet como plano B
         import flet as ft
         from ui_flet import main_flet
+
         ft.app(target=main_flet)
