fd0f7373a0c5baf138f1f251ca2d1f22288a2d56
fabiorv50 <fabiorv50@gmail.com>
2026-08-02 19:29:45 -0300

    Limpando conflitos de merge do main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import os
 import sys
 
-# Detecta se está rodando dentro do aplicativo Android
+# Detecta se está rodando no Android
 IS_MOBILE = (
         "ANDROID_ARGUMENT" in os.environ
         or "PYTHONOPTIMIZE" in os.environ
@@ -10,49 +9,21 @@ IS_MOBILE = (
 )
 
 if IS_MOBILE:
-    # --- AMBIENTE ANDROID (CELULAR) ---
+    # Interface Flet para o celular
     import flet as ft
     from ui_flet import main_flet
 
     ft.app(target=main_flet)
 
 else:
-    # --- AMBIENTE DESKTOP (WINDOWS / PYCHARM) ---
-=======
-# main.py
-import os
-import sys
-
-# Detecta se é Android/Mobile ou Desktop
-IS_MOBILE = (
-    "ANDROID_ARGUMENT" in os.environ 
-    or "PYTHONOPTIMIZE" in os.environ 
-    or "ANDROID_ROOT" in os.environ
-)
-
-if IS_MOBILE:
-    # No Android, carrega DIRETO a interface Flet
-    import flet as ft
-    from ui_flet import main_flet
-    ft.app(target=main_flet)
-else:
-    # No Windows/Desktop, carrega a interface CustomTkinter
->>>>>>> 190c7c764260e047a6a47ffe88cfa11e49f29afc
+    # Interface CustomTkinter para o PC (Windows)
     try:
         from ui_desktop import AppDesktop
 
         app = AppDesktop()
         app.mainloop()
-<<<<<<< HEAD
-    except Exception as e:
-        # Caso falhar o desktop no PC, ele usa o Flet como plano B
+    except Exception:
         import flet as ft
         from ui_flet import main_flet
 
         ft.app(target=main_flet)
-=======
-    except Exception:
-        import flet as ft
-        from ui_flet import main_flet
-        ft.app(target=main_flet)
->>>>>>> 190c7c764260e047a6a47ffe88cfa11e49f29afc
